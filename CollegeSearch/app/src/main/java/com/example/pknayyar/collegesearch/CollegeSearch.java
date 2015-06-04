db4837b9b5e55ea9accec68196dc5f4e02fa36e6
Pawanjit <pawanjit.nayyar@gmail.com>
2015-06-04 15:09:16 -0700

    Added the spinner
    Added spinner

--- CollegeSearch/app/src/main/java/com/example/pknayyar/collegesearch/CollegeSearch.java
+++ CollegeSearch/app/src/main/java/com/example/pknayyar/collegesearch/CollegeSearch.java
@@ -1,39 +1,29 @@
 package com.example.pknayyar.collegesearch;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
+
+public class CollegeSearch extends Activity {
 
 
-public class CollegeSearch extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_college_search);
+
+
+
+
+
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_college_search, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
